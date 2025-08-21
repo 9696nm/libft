433c894d2ad42998a66102e9a675f24480797320
hana/mori <sagiri.mori@gmail.com>
2025-08-21 21:49:00 +0900

    :wrench: print_bits ヘッダ追加

--- ft/stdio.h
+++ ft/stdio.h
@@ -6,12 +6,13 @@
 /*   By: hana/hmori <hmori@student.42tokyo.jp>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 12:33:33 by hana/hmori        #+#    #+#             */
-/*   Updated: 2025/07/26 13:41:58 by hana/hmori       ###   ########.fr       */
+/*   Updated: 2025/08/21 21:48:53 by hana/hmori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <stddef.h>
 #include <stdint.h>
 
 /* Non-standard */
@@ -22,5 +23,6 @@ void	ft_putstr_fd(char *s, int fd);
 
 /* Extra */
 void	ft_putbit(unsigned long int num, uint8_t bytesize);
+void	print_bits(const void *ptr, size_t byte);
 int		perrturn(int status, char *msg);
 int		ret_errmsg(int code, const char *msg);
